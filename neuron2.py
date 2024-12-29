7832b80aefab2e13ac4bb8e2fd9339a0b77b22ee
Tareq M <60986074+vivo1347@users.noreply.github.com>
2024-12-29 13:03:02 -0500

    Update neuron2.py

--- neuron2.py
+++ neuron2.py
@@ -6,7 +6,7 @@ import math
 data = {
     "x1": [0, 0, 1, 1],  # First input
     "x2": [0, 1, 0, 1],  # Second input
-    "y": [0, 1, 1, 0]    # Output (AND of x1 and x2)
+    "y": [0, 0, 0, 1]    # Output (AND of x1 and x2)
 }
 
 # Create a DataFrame from the data
@@ -36,7 +36,6 @@ w2=1
 b1=1
 b2=1
 w11=1
-b11=1
 cost =10
 lr=7.5
 while cost>0.00001:
@@ -47,14 +46,13 @@ while cost>0.00001:
     sum_dcost_b1=0
     sum_dcost_b2=0
     sum_dcost_dw11=0
-    sum_dcost_b11=0
     for i, row in data.iterrows():
         x1_i=row['x1']
         x2_i=row['x2']
         y_i=row['y']
-        z11=(x1_i*w1+b1)+(x2_i*w2+b2)
+        z11=(x1_i*w1)+(x2_i*w2)+b1
         a11=(1/(1+math.exp(-z11)))
-        z21=a11*w11+b11
+        z21=(a11*w11)+b2
         a21=(1/(1+math.exp(-z21)))
         N +=1
         sum_cost +=((y_i-a21)**2)
@@ -63,23 +61,21 @@ while cost>0.00001:
         sum_dcost_b1+=(-2*(y_i-a21)*a21*(1-a21)*a11*(1-a11)*1)
         sum_dcost_b2+=(-2*(y_i-a21)*a21*(1-a21)*a11*(1-a11)*1)
         sum_dcost_dw11+= (-2*(y_i-a21)*a21*(1-a21)*a11)
-        sum_dcost_b11+= (-2*(y_i-a21)*a21*(1-a21)*1)
     cost =(1/N)*sum_cost
     w1 -=(lr*sum_dcost_dw1)
     w2-=(lr*sum_dcost_dw2)
     b1-=(lr*sum_dcost_b1)
     b2-=(lr*sum_dcost_b2)
     w11 -=(lr*sum_dcost_dw11)
-    b11-=(lr*sum_dcost_b11)
     Epoch+=1
-    print(f"Epoch: {Epoch}, cost: {cost}, w1: {w1},w2: {w2},b1: {b1}, b2: {b2}, w11: {w11}, b11{b11}")
+    print(f"Epoch: {Epoch}, cost: {cost}, w1: {w1},w2: {w2},b1: {b1}, b2: {b2}, w11: {w11}")
 
 
 X1_ask= 0
 X2_ask= 1
-z_ask11=(X1_ask*w1+b1)+(X2_ask*w2+b2)
+z_ask11=(X1_ask*w1)+(X2_ask*w2)+b1
 a_ask11= (1/(1+math.exp(-z_ask11)))
-z_ask21=a_ask11*w11+b11
+z_ask21=(a_ask11*w11)+b2
 a_ask21= (1/(1+math.exp(-z_ask21)))
 print(f"given {X1_ask} and {X2_ask}\n")
 print(f"The output is: {a_ask21:.2f}")
